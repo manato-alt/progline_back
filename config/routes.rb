f6a15966340e655f46de3b4bdef2a55ec5b004ff
manato-alt <cococara988@gmail.com>
2024-04-16 09:56:11 +0900

    add:　削除用のルートを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "categories" => "categories#index"
   post "categories" => "categories#create"
+  delete 'categories/:user_id/:category_id' => 'categories#delete'
   post "users" => "users#create"
   get "term_registrations" => "term_registrations#index"
   post "term_registrations" => "term_registrations#create"
