cebc0277d0f910ad5d52bb1eaf1c7b8f47f019b4
manato-alt <cococara988@gmail.com>
2024-04-20 20:11:10 +0900

    add: アソシエーションを追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ApplicationRecord
   has_many :term_registrations
   has_many :users, through: :term_registrations
   has_one_attached :image
+  has_many :service_registrations
+  has_many :services, through: :service_registrations
 end
