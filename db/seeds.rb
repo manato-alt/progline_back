f3438ab441700ddbd4dea073978fe8ccd5762bff
manato-alt <cococara988@gmail.com>
2024-05-06 15:08:38 +0900

    add: テンプレートcategory,serviceの初期値を作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,16 @@
+require 'httparty'
+
 service_names = ['Youtube', 'Udemy', 'Qiita', 'Zenn', '書籍']
 
 # サービスを登録
 service_names.each do |name|
-  Service.create!(name: name)
+  TemplateService.create!(name: name)
+end
+
+response = HTTParty.get("https://qiita.com/api/v2/tags?page=1&per_page=30&sort=count")
+tags_data = response.parsed_response
+
+# 上位30件のタグデータを取得してデータベースに保存
+tags_data.first(30).each do |tag|
+  TemplateCategory.create(name: tag['id'], image_url: tag['icon_url'])
 end
