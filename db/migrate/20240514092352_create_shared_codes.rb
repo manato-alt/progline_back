c32974c3321eb76b38552ba10924e90f2fd8f170
manato-alt <cococara988@gmail.com>
2024-05-16 18:19:26 +0900

    add: shared_codesテーブルを作成

---
+++ db/migrate/20240514092352_create_shared_codes.rb
@@ -0,0 +1,11 @@
+class CreateSharedCodes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :shared_codes do |t|
+      t.string :public_name
+      t.string :code, unique: true
+      t.references :user, index: {:unique=>true}, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
