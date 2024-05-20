a6b5197e4df84f2aa5a694ab5f231fbe385d41da
manato-alt <cococara988@gmail.com>
2024-05-20 11:45:48 +0900

    add: バリデーションを追加

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,4 +1,8 @@
 class Content < ApplicationRecord
   belongs_to :service
   has_one_attached :image
+
+  validates :title, presence: true
+  validates :title, length: { maximum: 100 }
+  validates :url, length: { maximum: 2048 }
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -2,4 +2,6 @@ class Service < ApplicationRecord
   has_one_attached :image
   belongs_to :category
   has_many :contents, dependent: :destroy
+
+  validates :name, length: { in: 1..20 }
 end
