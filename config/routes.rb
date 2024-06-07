bbf4e191e83bd0149bd56379eb30543bb1439852
manato-alt <cococara988@gmail.com>
2024-06-07 15:46:52 +0900

    add: ローカル用の画像保存を追跡対象外にした。PublicName用の処理を作成

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,7 @@ Rails.application.routes.draw do
   get 'shared_codes/term_index', to: 'shared_codes#term_index'
   get 'shared_codes/service_index', to: 'shared_codes#service_index'
   get 'shared_codes/graph', to: 'shared_codes#graph'
+  get 'shared_codes/share_user', to: 'shared_codes#share_user'
   post "shared_codes/validate_access_term" => "shared_codes#validate_access_term"
   post "shared_codes/validate_access" => "shared_codes#validate_access"
 
